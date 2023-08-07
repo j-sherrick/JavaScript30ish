68b0b189374aaf6eb0aea8d6033fe61cef1198bf
Jason Sherrick <jasonsherrick@gmail.com>
2023-08-06 20:55:50 -0500

    Animated ornate clock

--- 02 - JS Grandfather Clock/assets/js/animate-clock.js
+++
@@ -1,6 +0,0 @@
-function getSVG(obj)
-{
-    console.log(obj.contentDocument);
-
-    console.log(obj.contentDocument.querySelector('#minutes'));
-}
---
+++ 02 - JS Ornate Clock/assets/js/animate-clock.js
@@ -0,0 +1,30 @@
+
+function runClock(hourHand, minuteHand, secondHand) 
+{
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secDegrees = ((seconds / 60) * 360);
+    secondHand.setAttribute('transform', `rotate(${secDegrees})`);
+    
+    const minutes = now.getMinutes();
+    const minDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6);
+    minuteHand.setAttribute('transform', `rotate(${minDegrees})`);
+    
+    const hours = now.getHours();
+    const hourDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30);
+    hourHand.setAttribute('transform', `rotate(${hourDegrees})`);
+}
+
+window.onload = () => {
+    const clock = document.querySelector('#clock').contentDocument;
+    const minuteHand = clock.querySelector("#minutes");
+    const secondHand = clock.querySelector('#seconds');
+    const hourHand = clock.querySelector('#hours');
+    
+    setInterval(runClock, 1000, hourHand, minuteHand, secondHand);
+    runClock(hourHand, minuteHand, secondHand);
+}
+
+// TODO: Make a Clock class with methods to update hand position
+// TODO: Inline javascript animation so clock runs with 'file://' protocol, i.e. without a server
